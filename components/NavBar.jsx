7014e3938ca7023bf79d03804b3433af9c6612b5
bryanleezh <leebryan307@gmail.com>
2024-01-24 01:18:18 +0100

    Added NavBar

---
+++ components/NavBar.jsx
@@ -0,0 +1,119 @@
+'use client';
+
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <div className="fixed bottom-0 w-full h-16 bg-white dark:bg-gray-900 border-t border-gray-200 dark:border-gray-800 flex items-center justify-around">
+      <Link className="flex flex-col items-center" href="/home">
+        {/* Icon can be changed the custom SVG that we drew */}
+        <HomeIcon className="h-6 w-6" />
+        <span className="text-xs">Home</span>
+      </Link>
+      <Link className="flex flex-col items-center" href="/zoo">
+        {/* Icon can be changed the custom SVG that we drew */}
+        <DogIcon className="h-6 w-6" />
+        <span className="text-xs">Zoo</span>
+      </Link>
+      <Link className="flex flex-col items-center" href="/shop">
+        {/* Icon can be changed the custom SVG that we drew */}
+        <ShoppingBagIcon className="h-6 w-6" />
+        <span className="text-xs">Shop</span>
+      </Link>
+      <Link className="flex flex-col items-center" href="/stats">
+        {/* Icon can be changed the custom SVG that we drew */}
+        <BarChartIcon className="h-6 w-6" />
+        <span className="text-xs">Stats</span>
+      </Link>
+    </div>
+  )
+}
+
+function BarChartIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <line x1="12" x2="12" y1="20" y2="10" />
+      <line x1="18" x2="18" y1="20" y2="4" />
+      <line x1="6" x2="6" y1="20" y2="16" />
+    </svg>
+  )
+}
+
+
+function DogIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M10 5.172C10 3.782 8.423 2.679 6.5 3c-2.823.47-4.113 6.006-4 7 .08.703 1.725 1.722 3.656 1 1.261-.472 1.96-1.45 2.344-2.5" />
+      <path d="M14.267 5.172c0-1.39 1.577-2.493 3.5-2.172 2.823.47 4.113 6.006 4 7-.08.703-1.725 1.722-3.656 1-1.261-.472-1.855-1.45-2.239-2.5" />
+      <path d="M8 14v.5" />
+      <path d="M16 14v.5" />
+      <path d="M11.25 16.25h1.5L12 17l-.75-.75Z" />
+      <path d="M4.42 11.247A13.152 13.152 0 0 0 4 14.556C4 18.728 7.582 21 12 21s8-2.272 8-6.444c0-1.061-.162-2.2-.493-3.309m-9.243-6.082A8.801 8.801 0 0 1 12 5c.78 0 1.5.108 2.161.306" />
+    </svg>
+  )
+}
+
+
+function HomeIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+      <polyline points="9 22 9 12 15 12 15 22" />
+    </svg>
+  )
+}
+
+
+function ShoppingBagIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M6 2 3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4Z" />
+      <path d="M3 6h18" />
+      <path d="M16 10a4 4 0 0 1-8 0" />
+    </svg>
+  )
+}
