8f329d2382676e781e944189a09614c70b6960d1
bryanleezh <leebryan307@gmail.com>
2024-01-31 15:43:27 +0100

    Feat: Add ProgressTracker component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
+'use client';
+
+import React from 'react';
 import NavBar from "@/components/NavBar";
+import ProgressTracker from '@/components/ProgressTracker';
 
 export default function Home() {
   return (
     <div>
       Home
+      <ProgressTracker />
       <NavBar />
     </div>
   );
