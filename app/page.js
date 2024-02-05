593e6507976e484c97b69800e71c5137de54699f
bryanleezh <leebryan307@gmail.com>
2024-02-05 13:52:13 +0100

    Chore: Remove imports

--- app/page.js
+++ app/page.js
@@ -3,7 +3,6 @@
 import React from 'react';
 import NavBar from "@/components/NavBar";
 import ProgressTracker from '@/components/ProgressTracker';
-import Image from 'next/image';
 
 export default function Home() {
   return (
